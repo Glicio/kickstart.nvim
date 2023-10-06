798634d7f47cf1fd97effd21d4868d03146bfecd
Glicio <glicioo@outlook.com>
2023-10-06 12:08:54 -0300

    4 spaces tab

--- init.lua
+++ init.lua
@@ -43,6 +43,16 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- custom config
+vim.opt.relativenumber = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+
+
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
@@ -267,6 +277,10 @@ vim.o.termguicolors = true
 
 -- my keymaps
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- thanks primeagean
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
